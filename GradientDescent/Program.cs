823743a12d46d271a0a1d283eeba9cecfdd2dab0
Igor Ladnik <ladnik@hotmail.com>
2019-11-14 07:37:23 +0200

    Initial commit

---
+++ GradientDescent/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using GradientDescentStd;
+
+namespace GradientDescentApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dx = 0.05;
+
+            var t = GradientDescent.SteepestDescent(func: x => 10 * x[0] * x[0] + x[1] * x[1],
+                                                      initX: new[] { 10.0, 10.0 },
+                                                      dx: new[] { dx, dx },
+                                                      precision: 1e-4);
+
+            Console.WriteLine($"SteepestDescent approx.:  {t.Item2} steps, x0 = {t.Item1[0]}, x1 = {t.Item1[1]}");
+
+            t = GradientDescent.SteepestDescent(func: x => 10 * x[0] * x[0] + x[1] * x[1],
+                                                      initX: new[] { 10.0, 10.0 },
+                                                      dx: new[] { dx, dx },
+                                                      precision: 1e-4,
+                                                      deriv: new Func<double[], double>[]
+                                                      {
+                                                          x => 20 * x[0],
+                                                          x =>  2 * x[1],
+                                                      });
+
+            Console.WriteLine($"SteepestDescent accurate: {t.Item2} steps, x0 = {t.Item1[0]}, x1 = {t.Item1[1]}");
+
+            t = GradientDescent.ConstStep(func: x => 10 * x[0] * x[0] + x[1] * x[1],
+                                                initX: new[] { 10.0, 10.0 },
+                                                dx: new[] { dx, dx },
+                                                precision: 1e-4);
+
+            Console.WriteLine($"ConstStep approx.:        {t.Item2} steps, x0 = {t.Item1[0]}, x1 = {t.Item1[1]}");
+
+            t = GradientDescent.ConstStep(func: x => 10 * x[0] * x[0] + x[1] * x[1],
+                                                initX: new[] { 10.0, 10.0 },
+                                                dx: new[] { dx, dx },
+                                                precision: 1e-4,
+                                                deriv: new Func<double[], double>[]
+                                                {
+                                                    x => 20 * x[0],
+                                                    x =>  2 * x[1],
+                                                });
+
+            Console.WriteLine($"ConstStep accurate:       {t.Item2} steps, x0 = {t.Item1[0]}, x1 = {t.Item1[1]}");
+
+            Console.WriteLine("\nPress any key to quit...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ GradientDescentStd/GradientDescent.cs
@@ -0,0 +1,102 @@
+﻿using System;
+
+namespace GradientDescentStd
+{
+    public static class GradientDescent
+    {
+        public static Tuple<double[], int> ConstStep(Func<double[], double> func, double[] initX, double[] dx, double precision,
+                                                     Func<double[], double>[] deriv = null)
+        {
+            var currX = initX;
+            var currPrecision = double.MaxValue;
+            var prevFunc = func(initX);
+
+            int k;
+            for (k = 0; currPrecision > precision; k++)
+            {
+                // Gradient
+                var gradient = deriv == null
+                                    ? GetGradient(func, prevFunc, currX, dx)
+                                    : GetGradient(deriv, currX);
+
+                // Next point
+                for (var i = 0; i < initX.Length; i++)
+                    currX[i] -= dx[i] * gradient[i];
+
+                // Func
+                var currFunc = func(currX);
+                currPrecision = Math.Abs(currFunc - prevFunc);
+                prevFunc = currFunc;
+            }
+
+            return new Tuple<double[], int>(currX, k);
+        }
+
+        public static Tuple<double[], int> SteepestDescent(Func<double[], double> func, double[] initX, double[] dx, double precision,
+                                                           Func<double[], double>[] deriv = null)
+        {
+            var currX = initX;
+            var currPrecision = double.MaxValue;
+            var prevFunc = func(initX);
+
+            var changeGradient = true;
+            double[] gradient = null;
+            var prevX = new double[initX.Length];
+            int k;
+            for (k = 0; currPrecision > precision; k++)
+            {
+                // Gradient
+                if (changeGradient)
+                    gradient = deriv == null
+                        ? GetGradient(func, prevFunc, currX, dx)
+                        : GetGradient(deriv, currX);
+
+                // Next point
+                for (var i = 0; i < initX.Length; i++)
+                {
+                    prevX[i] = currX[i];
+                    currX[i] -= dx[i] * gradient[i];
+                }
+
+                // Func
+                var currFunc = func(currX);
+                currPrecision = Math.Abs(currFunc - prevFunc);
+
+                if (changeGradient = currFunc > prevFunc)
+                {
+                    for (var i = 0; i < initX.Length; i++)
+                        currX[i] = prevX[i];
+                }
+                else
+                    prevFunc = currFunc;
+            }
+
+            return new Tuple<double[], int>(currX, k);
+        }
+
+        private static double[] GetGradient(Func<double[], double> func, double prevFunc, double[] x, double[] dx)
+        {
+            var gradient = new double[x.Length];
+            var xD = new double[x.Length];
+            for (var i = 0; i < x.Length; i++)
+            {
+                for (var j = 0; j < x.Length; j++)
+                    xD[j] = x[j];
+
+                xD[i] += dx[i];
+                gradient[i] = (func(xD) - prevFunc) / dx[i];
+            }
+
+            return gradient;
+        }
+
+        private static double[] GetGradient(Func<double[], double>[] deriv, double[] x)
+        {
+            var gradient = new double[x.Length];
+            for (var i = 0; i < x.Length; i++)
+                gradient[i] = deriv[i](x);
+
+            return gradient;
+        }
+    }
+}
